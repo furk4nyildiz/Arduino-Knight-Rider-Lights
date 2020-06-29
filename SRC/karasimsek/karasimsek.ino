bd5124fc547023c8a6bf8a8cd7c596b1657609d8
furk4nyildiz <56441541+furk4nyildiz@users.noreply.github.com>
2020-06-29 05:19:56 +0300

    Create karasimsek.ino

---
+++ SRC/karasimsek/karasimsek.ino
@@ -0,0 +1,16 @@
+void setup() {
+for (int i = 8; i <= 13 ; i++) { 
+  pinMode(i, OUTPUT);
+}
+}
+
+void loop() {
+for (int i = 8; i <= 13 ; i++) { 
+  digitalWrite(i, HIGH); 
+  delay(1000); 
+}
+for (int i = 13; i >= 8 ; i--) { 
+  digitalWrite(i, LOW); 
+  delay(1000);
+}
+}
